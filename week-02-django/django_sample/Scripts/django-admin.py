f358a4b99587afdc5f34e792d93410e1a1bf86b6
DESKTOP-550L6OS\leeap <leeap1004@naver.com>
2018-12-23 20:22:00 +0900

    django test

---
+++ week-02-django/django_sample/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\leeap\dev\coding-for-test\week-02-django\django_sample\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

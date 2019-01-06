595f1d8b77721e80ecd2ca37cc3722edb94baec7
DESKTOP-550L6OS\leeap <leeap1004@naver.com>
2019-01-06 23:22:52 +0900

    Add Css, Model, Database basic

--- week-02-django/src/sample/settings.py
+++ week-02-django/src/sample/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my apps
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -120,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    # '/var/www/static/',
+]
